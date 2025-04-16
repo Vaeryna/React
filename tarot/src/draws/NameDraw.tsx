c89ecf90047fa36327353899b86cf04042b71f37
Melanie <leblond.melanie@gmail.com>
2025-04-16 23:56:44 +0200

    got tone card

--- tarot/src/draws/NameDraw.tsx
+++ tarot/src/draws/NameDraw.tsx
@@ -1,28 +1,33 @@
 // tirage d'autant de cartes que de lettre dans le prénom
-
-import {useState} from "react";
-import confusing from "../data/confusing.json"
-import absurd from "../data/absurd.json"
-import trash from "../data/trash.json"
+import {useEffect, useState} from "react";
+import {useLocation} from "react-router-dom";
 
 export function Cards() {
-    const [card, setCard] = useState("");
-    
-    // if ()
-    
+    const [card, setCard] = useState({});
+
     return (<>
-
-
     </>)
 }
 
 
+function shuffle(card: { majeures: any[]; mineurs: { couleur: any, element: any, cartes: any[] } [] }) {
+    const allCards = [...card.majeures, ...card.mineurs.flatMap((m: any) => m.cartes)]
+
+    console.log("toutes les cartes", allCards)
+}
+
+
 export function NameDraw() {
+    const location = useLocation();
+    const {tone, userName} = location.state
+
+    useEffect(() => (console.log("tone", tone)))
+
+
     return (
         <>
             <h1> Bienvenue </h1>
 
-            Cards()
         </>
     )
 }
