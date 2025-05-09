a0b4e9cf5c5507b9983b9b5ba587137a07f19dea
Melanie <leblond.melanie@gmail.com>
2025-05-09 22:25:34 +0200

    draw result ok

--- tarot/src/views/draw-result.tsx
+++ tarot/src/views/draw-result.tsx
@@ -8,38 +8,42 @@ export function DrawResult({drawnCards, tone, isFlipped}: DrawResultProps) {
             <h1 className="text-center"> Voici votre tirage ...</h1>
             {drawnCards &&
                 (
-                    <div className="btn-group"  role="group" aria-label="cards">
+                    <div className="btn-group" role="group" aria-label="cards">
+                        <div className="card-image">
+                            {
+                                isFlipped.map((id) => {
+                                    const card = drawnCards[id]
+
+                                    return card ?
+                                        (<div className="card">
+                                            <img key={id} src={`/assets/cards-${tone}/${card.id}.png`}
+                                                 className="card-img"
+                                                 alt={`selectedCard ${card.name}`} id={`${card.id}`}/>
+                                        </div>)
+                                        : null
+                                })
+                            }
+                        </div>
+
                         {
                             isFlipped.map((id) => {
                                 const card = drawnCards[id]
 
                                 return card ?
-                                    (<div className="card">
-                                        <img key={id} src={`/assets/cards-${tone}/${card.id}.png`} className="card-img"
-                                             alt={`selectedCard ${card.name}`} id={`${card.id}`}/>
-                                    </div>)
-                                    : null
-                            })
-                        }
-
-                        {
-                            isFlipped.map((id) => {
-                                const card = drawnCards[id]
-
-                                return card ?
-                                    (<div >
+                                    (<div>
                                         <p className="text-center"> {card.interpretation}</p>
                                     </div>)
                                     : null
                             })
-                        }
+                        }<div/>
 
-                    </div>
+                </div>
 
                 )}
-        </div>
 
-    )
+</div>
+
+)
 }
 
 
