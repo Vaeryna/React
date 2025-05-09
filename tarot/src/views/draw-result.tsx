caa244141dc9f5bfad60bf3d31697708ffeab427
Melanie <leblond.melanie@gmail.com>
2025-05-09 22:14:54 +0200

    bg

--- tarot/src/views/draw-result.tsx
+++ tarot/src/views/draw-result.tsx
@@ -1,7 +1,6 @@
 import {DrawResultProps} from "../Data/DrawResultProps";
 
 
-
 export function DrawResult({drawnCards, tone, isFlipped}: DrawResultProps) {
 
     return (
@@ -23,6 +22,7 @@ export function DrawResult({drawnCards, tone, isFlipped}: DrawResultProps) {
                                     : null
                             })
                         } </div>
+
 
 
                 )}
